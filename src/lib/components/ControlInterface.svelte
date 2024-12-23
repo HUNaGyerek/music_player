0ee1951abf70f732086fb9772dec0fced4fc8e50
Oliver <rosszbikalo@gmail.com>
2024-12-23 23:26:23 +0100

    Export ControlInterface to a component

---
+++ src/lib/components/ControlInterface.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { ListMusic, Shuffle, Pause, Play, SkipBack, SkipForward } from 'lucide-svelte';
+	import VolumeBar from '$lib/components/VolumeBar.svelte';
+
+	let isPlaying = $state();
+	function togglePlaying() {
+		isPlaying = !isPlaying;
+	}
+
+	function handlePause() {
+		togglePlaying();
+	}
+
+	function handleResume() {
+		togglePlaying();
+	}
+</script>
+
+<div class="flex">
+	<div class="flex w-full justify-start gap-2 ps-10">
+		<button><ListMusic class="size-5" strokeWidth={3} fill="white" /></button>
+		<button><Shuffle class="size-5" strokeWidth={3} fill="white" /></button>
+	</div>
+	<div class="flex w-full justify-center gap-2">
+		<button><SkipBack strokeWidth={3} fill="white" /></button>
+		{#if isPlaying}
+			<button onclick={handlePause}><Pause class="size-5" strokeWidth={3} fill="white" /></button>
+		{:else}
+			<button onclick={handleResume}><Play class="size-5" strokeWidth={3} fill="white" /></button>
+		{/if}
+		<button><SkipForward class="size-5" strokeWidth={3} fill="white" /></button>
+	</div>
+	<div class="flex w-full justify-end pe-10">
+		<VolumeBar />
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,8 @@
 <script>
+	import ControlInterface from '$lib/components/ControlInterface.svelte';
 	import ProgressBar from '$lib/components/ProgressBar.svelte';
-	import VolumeBar from '$lib/components/VolumeBar.svelte';
+	import SongInformations from '$lib/components/SongInformations.svelte';
 	import WindowControls from '$lib/components/WindowControls.svelte';
-	import { ListMusic, Shuffle, Pause, Play, SkipBack, SkipForward } from 'lucide-svelte';
-
-	let isPlaying = $state();
-	function togglePlaying() {
-		isPlaying = !isPlaying;
-	}
-
-	function handlePause() {
-		togglePlaying();
-	}
-
-	function handleResume() {
-		togglePlaying();
-	}
 </script>
 
 <section>
@@ -25,35 +12,17 @@
 	</div>
 
 	<!-- Song Informations -->
-	<div data-tauri-drag-region class="flex select-none gap-4">
-		<img class="pointer-events-none h-full rounded-xl" src="https://placehold.co/80" alt="" />
-		<div class="pointer-events-none flex flex-col text-sm text-white">
-			<h2 class="text-base font-bold">asd</h2>
-			<p>Author:</p>
-			<p>00:00:00</p>
-		</div>
+	<div>
+		<SongInformations />
 	</div>
 
+	<!-- ProgressBar -->
 	<div class="mt-4 text-center">
 		<ProgressBar />
 	</div>
 
-	<div class="mt-1 flex text-white">
-		<div class="flex w-full justify-start gap-2 ps-10">
-			<button><ListMusic class="size-5" strokeWidth={3} fill="white" /></button>
-			<button><Shuffle class="size-5" strokeWidth={3} fill="white" /></button>
-		</div>
-		<div class="flex w-full justify-center gap-2">
-			<button><SkipBack strokeWidth={3} fill="white" /></button>
-			{#if isPlaying}
-				<button onclick={handlePause}><Pause class="size-5" strokeWidth={3} fill="white" /></button>
-			{:else}
-				<button onclick={handleResume}><Play class="size-5" strokeWidth={3} fill="white" /></button>
-			{/if}
-			<button><SkipForward class="size-5" strokeWidth={3} fill="white" /></button>
-		</div>
-		<div class="flex w-full justify-end pe-10">
-			<VolumeBar />
-		</div>
+	<!-- Song Control Interfaces -->
+	<div class="mt-1 text-white">
+		<ControlInterface />
 	</div>
 </section>
