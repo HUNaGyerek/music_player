d6e81c85cd60986c11fafe767558cdc1200424ce
Oliver <rosszbikalo@gmail.com>
2024-12-24 00:11:25 +0100

    Add async to switch music

--- src/lib/invoke.js
+++ src/lib/invoke.js
@@ -6,8 +6,8 @@ export function set_volume(value) {
 
 export const pause_audio = () => invoke('pause_audio');
 export const resume_audio = () => invoke('resume_audio');
-export const previous_track = () => invoke('previous_track');
-export const next_track = () => invoke('next_track');
+export const previous_track = async () => await invoke('previous_track');
+export const next_track = async () => await invoke('next_track');
 export const get_volume = () => invoke('get_volume');
 export const get_current_track_name = () => invoke('get_current_track_name');
 export const get_playlist_len = () => invoke('get_playlist_len');
