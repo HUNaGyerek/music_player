62489e0c128e2d662ac2532e4c257a601db07927
Oliver <rosszbikalo@gmail.com>
2024-12-23 23:28:37 +0100

    Add more api's

--- src/lib/invoke.js
+++ src/lib/invoke.js
@@ -4,6 +4,9 @@ export function set_volume(value) {
 	invoke('set_volume', { volume: value / 300 });
 }
 
-export function get_volume() {
-	return invoke('get_volume');
-}
+export const get_volume = () => invoke('get_volume');
+export const get_current_track_name = () => invoke('get_current_track_name');
+export const get_playlist_len = () => invoke('get_playlist_len');
+export const get_audio_length = async () =>
+	invoke('get_audio_length', { musicIndex: await get_current_music_index() });
+export const get_current_music_index = () => invoke('get_current_music_index');
