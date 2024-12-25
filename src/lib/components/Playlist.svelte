5063f8743c33d38b0609a40fc92f4e7b3683402a
Oliver <rosszbikalo@gmail.com>
2024-12-25 20:20:58 +0100

    Create playlist component

---
+++ src/lib/components/Playlist.svelte
@@ -0,0 +1,53 @@
+<script>
+	import {
+		get_current_track_informations,
+		get_playlist,
+		get_track_informations_by_index,
+		play_by_index
+	} from '$lib/invoke';
+	import { currentTrack } from '$lib/store';
+	import { convertSecondsToMinuteText } from '$lib/utils';
+	import { Play } from 'lucide-svelte';
+
+	async function handleChangeMusic(idx) {
+		play_by_index(idx);
+	}
+</script>
+
+<div class="scroll-container relative overflow-y-auto">
+	<div class="me-2">
+		{#await get_playlist() then playlist}
+			{#each playlist as music, idx}
+				<button
+					ondblclick={() => handleChangeMusic(idx)}
+					class="mt-2 flex h-auto w-full items-center rounded-lg px-4 py-2 text-xs font-semibold shadow-lg {$currentTrack.title ===
+					music.title
+						? 'bg-blue-400 text-white hover:bg-blue-500'
+						: 'text-black bg-gray-200 hover:bg-gray-400'}"
+				>
+					<div class="flex w-full flex-col items-start">
+						<h2 class="text-sm">{music.title}</h2>
+						<p>Author</p>
+					</div>
+
+					<div class="flex items-center justify-end gap-2">
+						<p>{convertSecondsToMinuteText(music.lenght)}</p>
+						<Play strokeWidth={3} color={$currentTrack.title === music.title ? 'white' : 'black'} />
+					</div>
+				</button>
+			{/each}
+		{/await}
+	</div>
+</div>
+
+<style lang="postcss">
+	.scroll-container::-webkit-scrollbar {
+		@apply w-1;
+	}
+	.scroll-container::-webkit-scrollbar-track {
+		@apply mt-2 rounded-full bg-gray-200;
+	}
+	.scroll-container::-webkit-scrollbar-thumb {
+		@apply rounded-full bg-black-600;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,20 @@
 <script>
 	import '../app.css';
+	import Playlist from '$lib/components/Playlist.svelte';
+	import { isMenuOpened } from '$lib/store';
+
 	let { children } = $props();
 </script>
 
-<main data-tauri-drag-region class="bg-black-500 h-screen w-screen overflow-hidden rounded-2xl p-4">
-	{@render children()}
-</main>
+<section class="flex h-screen flex-col overflow-x-hidden [&::-webkit-scrollbar]:hidden">
+	<main data-tauri-drag-region class="z-50 h-[204px] w-screen rounded-2xl bg-black-500 p-4">
+		{@render children()}
+	</main>
+	<div
+		class="{$isMenuOpened
+			? 'flex'
+			: 'hidden'} -mt-6 h-[calc(100vh-204px+24px)] w-screen flex-col rounded-2xl bg-black-400 px-4 pb-4 pt-6"
+	>
+		<Playlist />
+	</div>
+</section>
