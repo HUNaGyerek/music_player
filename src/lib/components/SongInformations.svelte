690b00f73b8144b90350d89665d6bc9c1b0d5142
Oliver <rosszbikalo@gmail.com>
2024-12-25 20:22:19 +0100

    Add working for music details

--- src/lib/components/SongInformations.svelte
+++ src/lib/components/SongInformations.svelte
@@ -1,18 +1,21 @@
 <script>
-	// import { get_current_track_informations } from '$lib/invoke';
+	import { get_current_track_informations } from '$lib/invoke';
 	import { currentTrack } from '$lib/store';
 	import { convertSecondsToMinuteText } from '$lib/utils';
+	import { onMount } from 'svelte';
 
-	// $effect(async () => {
-	// 	console.log($currentTrack);
-	// });
+	onMount(async () => {
+		$currentTrack = await get_current_track_informations();
+	});
 </script>
 
 <div data-tauri-drag-region class="flex select-none gap-4">
-	<img class="pointer-events-none h-full rounded-xl" src="https://placehold.co/80" alt="" />
-	<div class="pointer-events-none flex flex-col text-sm text-white">
-		<h2 class="text-base font-bold">{$currentTrack.title || 'asd'}</h2>
-		<p>Author: {$currentTrack.author || ''}</p>
-		<p>{convertSecondsToMinuteText($currentTrack.lenght) || '00:00:00'}</p>
-	</div>
+	{#if $currentTrack}
+		<img class="pointer-events-none h-full rounded-xl" src="https://placehold.co/80" alt="" />
+		<div class="pointer-events-none flex flex-col text-sm text-white">
+			<h2 class="text-base font-bold">{$currentTrack.title || 'Not loaded'}</h2>
+			<p>Author: {$currentTrack.author || ''}</p>
+			<p>{convertSecondsToMinuteText($currentTrack.lenght) || '00:00:00'}</p>
+		</div>
+	{/if}
 </div>
