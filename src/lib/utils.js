ad460cb736e71d0a3cddc05ef880352b262a2971
Oliver <rosszbikalo@gmail.com>
2024-12-25 20:31:47 +0100

    Disable Context menu for release

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -6,3 +6,27 @@ export function convertSecondsToMinuteText(seconds) {
 
 export const convertMinuteTextToSeconds = (time) =>
 	time.split(':').reduce((s, t) => s * 60 + +t, 0);
+
+export function disableContextMenu() {
+	if (window.location.hostname !== 'tauri.localhost') {
+		return;
+	}
+
+	document.addEventListener(
+		'contextmenu',
+		(e) => {
+			e.preventDefault();
+			return false;
+		},
+		{ capture: true }
+	);
+
+	document.addEventListener(
+		'selectstart',
+		(e) => {
+			e.preventDefault();
+			return false;
+		},
+		{ capture: true }
+	);
+}
