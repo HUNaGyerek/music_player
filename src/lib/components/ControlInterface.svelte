7a123fb3290b698bd3d770dd1725207116070fef
Oliver <rosszbikalo@gmail.com>
2024-12-25 20:27:09 +0100

    Add functionality for every button

--- src/lib/components/ControlInterface.svelte
+++ src/lib/components/ControlInterface.svelte
@@ -1,18 +1,34 @@
 <script>
-	import { ListMusic, Shuffle, Pause, Play, SkipBack, SkipForward } from 'lucide-svelte';
+	import { ListMusic, Shuffle, Pause, Play, SkipBack, SkipForward, ListX, X } from 'lucide-svelte';
 	import VolumeBar from '$lib/components/VolumeBar.svelte';
 	import {
 		get_current_track_informations,
 		next_track,
 		pause_audio,
 		previous_track,
-		resume_audio
+		resume_audio,
+		shuffle_playlist,
+		toggle_playlist_menu
 	} from '$lib/invoke';
-	import { currentTrack } from '$lib/store';
+	import { currentTrack, isMenuOpened, isShuffled } from '$lib/store';
+	import { onMount } from 'svelte';
+	import { invoke } from '@tauri-apps/api/core';
+	import { listen } from '@tauri-apps/api/event';
 
 	let isPlaying = $state(true);
 	function togglePlaying() {
 		isPlaying = !isPlaying;
+	}
+
+	function toggleMenu() {
+		$isMenuOpened = !$isMenuOpened;
+		toggle_playlist_menu();
+	}
+
+	function toggleShuffle() {
+		$isShuffled = !$isShuffled;
+
+		shuffle_playlist();
 	}
 
 	function handlePause() {
@@ -38,8 +54,18 @@
 
 <div class="flex">
 	<div class="flex w-full justify-start gap-2 ps-10">
-		<button><ListMusic class="size-5" strokeWidth={3} fill="white" /></button>
-		<button><Shuffle class="size-5" strokeWidth={3} fill="white" /></button>
+		{#if !$isMenuOpened}
+			<button onclick={toggleMenu}><ListMusic class="size-6" strokeWidth={2} fill="white" /></button
+			>
+		{:else}
+			<button onclick={toggleMenu}><ListX class="size-6" strokeWidth={2} fill="white" /></button>
+		{/if}
+		<button onclick={toggleShuffle} class="relative flex flex-col justify-center">
+			<Shuffle class="size-5 {$isShuffled ? 'absolute left-4 scale-[60%]' : ''}" strokeWidth={3} />
+			{#if $isShuffled}
+				<X class="absolute" strokeWidth={3} />
+			{/if}
+		</button>
 	</div>
 	<div class="flex w-full justify-center gap-2">
 		<button onclick={handlePrevious}><SkipBack strokeWidth={3} fill="white" /></button>
