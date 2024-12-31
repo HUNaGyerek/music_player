31248faccc5f6464c6af782f25777fe02208aff0
Oliver <rosszbikalo@gmail.com>
2024-12-31 16:03:46 +0100

    Remove unused invoke

--- src/lib/invoke.js
+++ src/lib/invoke.js
@@ -19,8 +19,6 @@ export const get_current_audio_length = () => invoke('get_current_audio_length')
 export const get_playlist_len = () => invoke('get_playlist_len');
 export const get_current_music_index = () => invoke('get_current_music_index');
 export const get_current_track_informations = () => invoke('get_current_track_informations');
-export const get_track_informations_by_index = (idx) =>
-	invoke('get_track_informations_by_index', { idx });
 export const get_current_position = () => invoke('get_current_position');
 export const shuffle_playlist = () => invoke('shuffle_playlist');
 export const toggle_playlist_menu = () => invoke('toggle_playlist_menu');
